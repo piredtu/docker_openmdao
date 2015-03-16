c15a6b501e14158800aed9ddd3c389353eec4248
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-03-16 12:15:43 +0100

    Adding 0.12.0; moving things around

---
+++ 0.10.3.2/fix_dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf /usr/local/lib/python3.4/dist-packages/tornado*
+rm -rf /usr/local/lib/python3.4/dist-packages/IPython*
+rm -rf /usr/local/lib/python3.4/dist-packages/ipython*
+
+rm -rf /usr/local/lib/python2.7/dist-packages/tornado*
+rm -rf /usr/local/lib/python2.7/dist-packages/ipython*
+rm -rf /usr/local/lib/python2.7/dist-packages/IPython*
+
+rm -rf /usr/lib/python2.7/dist-packages/tornado*
+rm -rf /usr/lib/python2.7/dist-packages/ipython*
+rm -rf /usr/lib/python2.7/dist-packages/IPython*
+
+rm -rf $OPENMDAODIR/lib/python2.7/site-packages/IPython*
+rm -rf $OPENMDAODIR/lib/python2.7/site-packages/ipython*
+rm -rf $OPENMDAODIR/lib/python2.7/site-packages/tornado*
+
+bash -c "source $ACTIVATE; pip install -r $INSTALLDIR/requirements.txt"
--- notebook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Strict mode
-set -euo pipefail
-IFS=$'\n\t'
-
-bash -c 'source $OPENMDAODIR/bin/activate; ipython2 notebook --no-browser --port 8888 --ip=*  --matplotlib=inline'
